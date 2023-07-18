c17527fb86978b62705dd1f0da81f981d278ace0
Cicero Alysson <cicero.araujo@fcamara.com.br>
2023-07-18 12:08:16 -0300

    feat(config): add .env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppService } from './app.service';
 
+import { AppController } from './app.controller';
+
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
