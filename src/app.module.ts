ceecd6a38b831956cfb2c1f904489976b2efb331
Cicero Alysson <cicero.araujo@fcamara.com.br>
2023-07-18 14:49:50 -0300

    feat(database): configure database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppService } from './app.service';
 
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
